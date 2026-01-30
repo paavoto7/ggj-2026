fabd0dd476bc57625a1f8584e114c9841087e405
eerikpeerik <erik.astrom@abo.fi>
2026-01-30 21:55:02 +0200

    kakka

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -4,5 +4,4 @@ extends Control
 
 func _on_start_game_pressed() -> void:
 	print("BUTTON PRESSED")
-	var result = get_tree().change_scene_to_file("res://Scenes/levels/level1.tscn") # Replace with function body.
-	print("Result: ", result)
+	get_tree().change_scene_to_file("res://Scenes/levels/level1.tscn") # Replace with function body.
