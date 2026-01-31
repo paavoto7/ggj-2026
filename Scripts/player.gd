22b1eac3e07aee88248d54031658c1571061b793
Paavo Westerlund <103736913+paavoto7@users.noreply.github.com>
2026-01-31 13:22:16 +0200

    Added basic player animations
    Also some file deletison and moves

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,38 @@
-extends Node2D
+class_name Player extends CharacterBody2D
 
+@export var speed: float = 300.0
+@export var jump_velocity: float = -400.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-    pass # Replace with function body.
+@onready var animated_sprite := $AnimatedSprite2D as AnimatedSprite2D
 
+var direction: float = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-    pass
+func _physics_process(delta: float) -> void:
+    # Add the gravity.
+    if not is_on_floor():
+        velocity += get_gravity() * delta
+
+    # Handle jump.
+    if Input.is_action_just_pressed("jump") and is_on_floor():
+        velocity.y = jump_velocity
+        if direction < 0:
+            _play_anim("jump_left")
+        else:
+            _play_anim("jump_right")
+
+    # Get the input direction and handle the movement/deceleration.
+    # As good practice, you should replace UI actions with custom gameplay actions.
+    direction = Input.get_axis("move_left", "move_right")
+    if direction:
+        velocity.x = direction * speed
+        if direction < 0:
+            _play_anim("walk_left")
+        else:
+            _play_anim("walk_right")
+    else:
+        velocity.x = move_toward(velocity.x, 0, speed)
+
+    move_and_slide()
+
+func _play_anim(anim_name: StringName) -> void:
+    animated_sprite.play(anim_name)
