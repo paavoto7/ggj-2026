248dbe538d019520a0c1653a17188b665d2fbc66
Paavo Westerlund <103736913+paavoto7@users.noreply.github.com>
2026-01-31 20:49:47 +0200

    Added hovering mask movement

--- Scripts/Interactables/Masks/WorldMaskHandler.gd
+++ Scripts/Interactables/Masks/WorldMaskHandler.gd
@@ -1,4 +1,9 @@
-class_name Mask extends Area2D
+class_name WorldMask extends Area2D
+
+@export var max_val: float = 1
+@export var min_val: float = -1
+@export var hover_speed: float = 2
+
 
 @onready var mask_item: MaskItem = $MaskItem as MaskItem
 
@@ -6,6 +11,14 @@ class_name Mask extends Area2D
 func _ready() -> void:  
     add_to_group("interactables")
     pass # Replace with function body.
+
+var elapsed_time: float = 0
+func _physics_process(delta: float) -> void:
+    elapsed_time += delta * hover_speed
+    var t = (sin(elapsed_time) + 1.0) * 0.5 # converts -1..1 → 0..1
+    var value = lerp(min_val, max_val, t)
+    position.y += value
+
 
 func interact(player: Player) -> void:
     # Remove from current parent before adding to inventory
