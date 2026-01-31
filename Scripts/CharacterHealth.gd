5791cfc39f8bf31f12d14c85b1b2215fb788ba3e
eerikpeerik <erik.astrom@abo.fi>
2026-02-01 00:35:48 +0200

    Implemented first version of falling spike.

--- Scripts/CharacterHealth.gd
+++ Scripts/CharacterHealth.gd
@@ -5,18 +5,20 @@ class_name CharacterHealth extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-    pass
+	pass
 
 func take_damage(amount: float) -> void:
-    health -= amount
-    if (health <= 0):
-        _died()
+	health -= amount
+	print("Player's current health:")
+	print(health)
+	if (health <= 0): 
+		_died()
 
 func _died() -> void:
-    get_parent().queue_free()
-    pass
+	get_parent().queue_free()
+	pass
