875df49b0bfd35478df730ee78762e6538d318f9
Paavo Westerlund <103736913+paavoto7@users.noreply.github.com>
2026-02-01 12:11:49 +0200

    First level mask

---
+++ Scripts/Interactables/Masks/MaskInstances/ForestMask.gd
@@ -0,0 +1,24 @@
+class_name ForestMask extends MaskItem
+
+var callables: Array[Callable] = []
+
+const time_scale_factor: int =  2
+
+func _exit_tree() -> void:
+    unset_static_effects()
+
+func get_callables() -> Array[Callable]:
+    return callables
+
+func set_static_effects():
+    Engine.time_scale *= time_scale_factor
+    print("Set static effects")
+
+func unset_static_effects():
+    Engine.time_scale /= time_scale_factor
+    print("Unset static effects")
+
+func _ready() -> void:
+    for method in get_method_list():
+        if method.name.begins_with("callable"):
+            callables.append(Callable(self, method.name))
