5433dc8ab60b7fb0225ff95fff8bb37a37a4ea7d
Paavo Westerlund <103736913+paavoto7@users.noreply.github.com>
2026-02-01 09:40:05 +0200

    Fixed code and assets

--- Scripts/Enemy/pingpongenemy.gd
+++ Scripts/Enemy/pingpongenemy.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @export var max_val: Vector2 = Vector2.ZERO
 @export var min_val: Vector2 = Vector2.ZERO
-@export var movement_speed: float = 80
+@export var movement_speed: float = 2
 @export var damage_amount: float = 25
 @export var pause_time: float = 0.3
 
@@ -14,33 +14,34 @@ var pause_timer: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	max_val.y = position.y
-	min_val.y = position.y
-	target = max_val
-	body_entered.connect(_on_body_enter)
+    max_val += position
+    min_val += position
+
+    max_val.y = position.y
+    min_val.y = position.y
+    
+    target = max_val
+    body_entered.connect(_on_body_enter)
 
 func _physics_process(delta: float) -> void:
-	if pause_timer > 0:
-		pause_timer -= delta
-		return
+    if pause_timer > 0:
+        pause_timer -= delta
+        return
 
-	var dir: Vector2 = (target - position)
-	var dist: float = dir.length()
+    position = position.lerp(target, movement_speed * delta)
 
-	if dist <= movement_speed * delta:
-		position = target
-		target = min_val if target == max_val else max_val
-		pause_timer = pause_time
-		anim_sprite.scale.x *= -1
-	else:
-		position += dir.normalized() * movement_speed * delta
+    if position.distance_to(target) < 1.0:
+        position = target
+        target = min_val if target == max_val else max_val
+        pause_timer = pause_time
+        anim_sprite.scale.x *= -1
 
-func _on_body_enter(body: CharacterBody2D) -> void:
-	# Remove from current parent before adding to inventory
-	
-	if body is not Player:
-		return
-	print("Hedgehod did damage")
-	
-	body.get_node("HealthNode").take_damage(damage_amount)
-	
+func _on_body_enter(body: Node2D) -> void:
+    # Remove from current parent before adding to inventory
+    
+    if body is not Player:
+        return
+    print("Hedgehod did damage")
+    
+    body.get_node("HealthNode").take_damage(damage_amount)
+    
