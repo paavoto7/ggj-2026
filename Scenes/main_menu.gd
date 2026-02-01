3bd8feaeeb9b7f9272f6bb99daf4aa7245218c31
eerikpeerik <erik.astrom@abo.fi>
2026-02-01 15:22:24 +0200

    Fixed broken button

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,5 +1,5 @@
 extends Control
 
-func _on_start_game_pressed() -> void:
+func _on_start_game_pressed():
 	print("BUTTON PRESSED")
-	get_tree().change_scene_to_file("res://Scenes/levels/hub.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/levels/hub.tscn")
