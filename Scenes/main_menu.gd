0ae3b659e5e32ae266887010cbb3d82083cf4134
eerikpeerik <erik.astrom@abo.fi>
2026-02-01 15:11:24 +0200

    Main menu now leads to hub world

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -4,19 +4,4 @@ extends Control
 
 func _on_start_game_pressed() -> void:
 	print("BUTTON PRESSED")
-	get_tree().change_scene_to_file("res://Scenes/levels/level4.tscn") # Replace with function body.
-
-func _on_start_game_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/levels/level2.tscn")
-
-
-func _on_start_game_3_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/levels/level3.tscn")
-
-
-func _on_start_game_4_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/levels/level4.tscn")
-
-
-func _on_start_game_5_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/levels/level5.tscn")
+	get_tree().change_scene_to_file("res://Scenes/levels/hub.tscn") # Replace with function body.
