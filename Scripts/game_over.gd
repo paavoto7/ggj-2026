f5e66bfa30b2f4734cbc8c59d47d8c72eafb4016
eerikpeerik <erik.astrom@abo.fi>
2026-02-01 15:41:21 +0200

    Added return to Main Menu and return to Hub buttons.

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -6,6 +6,12 @@ func _ready():
 func _on_retry_pressed() -> void:
 	get_tree().reload_current_scene()
 	
+func _on_back_to_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	
+func _on_hub_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/levels/hub.tscn")
+	
 func game_over():
 	self.show()
  # Replace with function body.
