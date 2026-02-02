14d7cb47f97c72f5ac3ce2c31ec2e6bbf22a3a67
Paavo Westerlund <103736913+paavoto7@users.noreply.github.com>
2026-02-02 19:03:48 +0200

    Added a quit game button

--- Scripts/Menus/main_menu.gd
+++ Scripts/Menus/main_menu.gd
@@ -3,3 +3,7 @@ extends MenuBase
 func _on_start_game_pressed():
     print("Loading hub scene")
     get_tree().change_scene_to_file("res://Scenes/levels/hub.tscn")
+
+func _on_quit_game_pressed():
+    print("Quitting game")
+    get_tree().quit()
