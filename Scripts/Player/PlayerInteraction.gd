2dc7105ba51560ed4bfb99cbdff1085372840dc2
Paavo Westerlund <103736913+paavoto7@users.noreply.github.com>
2026-02-02 15:55:14 +0200

    Added audio buses, web version works now

--- Scripts/Player/PlayerInteraction.gd
+++ Scripts/Player/PlayerInteraction.gd
@@ -10,6 +10,9 @@ func _ready() -> void:
 # Not ideal in any way, but works for now
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("interact"):
+		print("l1")
 		for body in get_overlapping_areas():
+			print("l2")
 			if body.is_in_group("interactables"):
+				print("l3")
 				body.interact(player)  # Call a method on the interactable
