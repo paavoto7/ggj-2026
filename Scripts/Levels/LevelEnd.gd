fe9a0253411b88584b570993c01c9382774a3a49
Paavo Westerlund <103736913+paavoto7@users.noreply.github.com>
2026-02-04 20:46:14 +0200

    Mask gets now added to game data on level finish

--- Scripts/Levels/LevelEnd.gd
+++ Scripts/Levels/LevelEnd.gd
@@ -1,5 +1,8 @@
 extends Interactable
 
+func interact(player: Player) -> void:
+    var current_mask: MaskItem = player.get_node_or_null("Inventory/MaskItem") as MaskItem
+    if current_mask:
+        MainManager.current_game_data.collected_masks[current_mask.mask_id] = true
 
-func interact(_player: Player) -> void:
     get_tree().change_scene_to_file.call_deferred("res://Scenes/levels/hub.tscn")
