53172981bbcb4efaeac3312d88ee57aa96996313
Paavo Westerlund <103736913+paavoto7@users.noreply.github.com>
2026-02-09 11:20:38 +0200

    Organised player script

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -35,69 +35,18 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
     if Input.is_action_just_pressed("pause_menu"):
         $"GameOver".game_over()
+        return
     
     if !should_update:
         return
         
     anim_playing = false
-    
-    coyote_timer -= delta
-    jump_buffer_timer -= delta
 
-    if is_on_floor():
-        coyote_timer = coyote_time
-
-    
-    # Add the gravity.
-    if not is_on_floor():
-        velocity += get_gravity() * delta
-
-    # Handle jump.
-    if Input.is_action_just_pressed("jump"):
-        jump_buffer_timer = jump_buffer_time
-
-        if is_on_floor():
-            velocity.y = jump_velocity
-            if direction < 0:
-                _play_anim("jump_left")
-            else:
-                _play_anim("jump_right")
-            jump_timer.start()
-            
-        if jump_buffer_timer > 0 and coyote_timer > 0 and not is_on_floor():
-            velocity.y = jump_velocity
-            jump_buffer_timer = 0
-            coyote_timer = 0
-            if direction < 0:
-                _play_anim("jump_left")
-            else:
-                _play_anim("jump_right")
-            jump_timer.start()
-
+    # Query for jump and jump if pressed    
+    _handle_jump(delta)
 
     # Get the input direction and handle the movement/deceleration.
-    # As good practice, you should replace UI actions with custom gameplay actions.
-    direction = Input.get_axis("move_left", "move_right")
-    if direction:
-        velocity.x = direction * speed
-        if !steps_sound.playing:
-            steps_sound.play()
-
-        if direction < 0:
-            _play_anim("walk_left")
-            dir_sign = Direction.Left
-            if attack_handler.position.x > 0:
-                attack_handler.position.x *= -1
-        else:
-            _play_anim("walk_right")
-            dir_sign = Direction.Right
-            if attack_handler.position.x < 0:
-                attack_handler.position.x *= -1
-    else:
-        velocity.x = move_toward(velocity.x, 0, speed)
-
-    if Input.is_action_just_pressed("attack"):
-        attack_handler.attack(dir_sign)
+    _handle_movement()
 
     if is_on_floor() and direction == 0:
         if dir_sign == Direction.Left:
@@ -111,6 +60,62 @@ func _physics_process(delta: float) -> void:
     _apply_callables()
     
     move_and_slide()
+
+
+func _handle_movement() -> void:
+    direction = Input.get_axis("move_left", "move_right")
+
+    velocity.x = direction * speed if direction else move_toward(velocity.x, 0, speed)
+    
+    if !steps_sound.playing:
+        steps_sound.play()
+    
+    if velocity:
+        if direction * dir_sign < 0:
+            attack_handler.position.x *= -1
+        
+        if direction < 0:
+            _play_anim("walk_left")
+            dir_sign = Direction.Left
+            
+        else:
+            _play_anim("walk_right")
+            dir_sign = Direction.Right
+
+
+func _handle_jump(delta: float) -> void:
+    coyote_timer -= delta
+    jump_buffer_timer -= delta
+
+    var on_floor: bool = is_on_floor()
+    
+    # Add the gravity.
+    if not on_floor:
+        velocity += get_gravity() * delta
+    else:
+        coyote_timer = coyote_time
+
+    # Handle jump.
+    if Input.is_action_just_pressed("jump"):
+        jump_buffer_timer = jump_buffer_time
+
+        if on_floor:
+            _jump()
+            
+        elif jump_buffer_timer > 0 and coyote_timer > 0:
+            jump_buffer_timer = 0
+            coyote_timer = 0
+            _jump()
+
+
+# Do the actual jump
+func _jump() -> void:
+    velocity.y = jump_velocity
+    if direction < 0:
+        _play_anim("jump_left")
+    else:
+        _play_anim("jump_right")
+    jump_timer.start()
 
 
 func _play_anim(anim_name: StringName) -> void:
